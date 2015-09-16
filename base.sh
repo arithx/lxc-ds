63d7469a9b259c83697401296a93023281427d45
Stephen Lowrie <stephen.lowrie@gmail.com>
2015-09-16 10:58:42 -0500

    update command

--- base.sh
+++ base.sh
@@ -1,6 +1,6 @@
 if [ ! -e "/usr/bin/lxc-create" ]; then
     apt-get update
-    apt-get install -qqy lxc
+    apt-get install lxc
 fi
 
 echo "lxc.aa_profile = unconfined" >> /etc/lxc/default.conf
@@ -8,5 +8,5 @@ echo "lxc.cgroup.devices.allow = c 10:237 rwm #loop-control" >> /etc/lxc/default
 echo "lxc.cgroup.devices.allow = b 7:* rwm # loop*" >> /etc/lxc/default.conf
 lxc-create -t ubuntu -n ds
 lxc-start -n ds -d
-lxc-attach -n ds -- echo "ubuntu" | sudo apt-get update -qqy wget
-lxc-attach -n ds -- echo "ubuntu" | sudo wget -O - https://raw.github.com/arithx/shstack/master/base.sh | sh
+lxc-attach -n ds -- bash -c "echo \"ubuntu\" | sudo apt-get update -qqy wget"
+lxc-attach -n ds -- bash -c "echo \"ubuntu\" | sudo wget -O - https://raw.github.com/arithx/shstack/master/base.sh | sh"
