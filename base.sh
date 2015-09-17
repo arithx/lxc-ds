e54ba21150efd0e4fae93167ee89cdaeada088fd
step6829 <stephen.lowrie@rackspace.com>
2015-09-17 15:00:44 -0500

    Added 5 second wait to workaround network issues

--- base.sh
+++ base.sh
@@ -9,6 +9,10 @@ echo "lxc.cgroup.devices.allow = b 7:* rwm # loop*" >> /etc/lxc/default.conf
 lxc-create -t ubuntu -n ds
 lxc-start -n ds -d
 lxc-wait -n ds -s RUNNING
+
+# For whatever reason the LXC container can have network issues immediately after setting to running state
+sleep 5
+
 lxc-attach -n ds -- bash -c "echo \"ubuntu\" | sudo -S apt-get update --fix-missing"
 lxc-attach -n ds -- bash -c "echo \"ubuntu\" | sudo -S apt-get install -qqy wget"
 lxc-attach -n ds -- bash -c "echo \"ubuntu\" | sudo -S wget -O - https://raw.github.com/arithx/shstack/master/base.sh | sh"
